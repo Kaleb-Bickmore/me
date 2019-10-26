5a7c2caf2ccf203301c134b12d0ca51486e65318
Kaleb Bickmore <kbic17@gmail.com>
2019-10-26 13:28:44 -0600

    fixing comment

--- personalwebsite/src/components/sidebar.js
+++ personalwebsite/src/components/sidebar.js
@@ -28,9 +28,12 @@ export default class Sidebar extends Component {
         			</nav>
 
         			<div class="colorlib-footer">
-        				{/* <p><small>&copy;  */}
-        {/* Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> */}
-       {/* </span> <span>Images: <a href="https://unsplash.com/" target="_blank">Unsplash.com</a></span></small></p> */}
+        			    <p><small>&copy;  
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | 
+        This template is made with 
+        <i class="icon-heart" aria-hidden="true"></i> by 
+        <a href="https://colorlib.com" target="_blank">Colorlib</a> 
+       <span>Images: <a href="https://unsplash.com/" target="_blank">Unsplash.com</a></span></small></p> 
         				<ul>
         					<li><a href="/#"><i class="icon-facebook2"></i></a></li>
         					<li><a href="/#"><i class="icon-twitter2"></i></a></li>
